8e3b6bc56569d197c7ad7fbf6afdde2ff8d88931
GuillaumeLoriot <loriot.guillaume@gmail.com>
2025-08-14 20:29:13 +0200

    installed slider library + implemented slider for review

--- src/app/components/review-slider/review-slider.component.ts
+++ src/app/components/review-slider/review-slider.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-review-slider',
   standalone: true,
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './review-slider.component.html',
   styleUrl: './review-slider.component.scss'
 })
 export class ReviewSliderComponent {
 
-  tests = [1,2,3];
+  tests = [1,2,3,4,5,6,7,8,9];
 
 }
