2567d137658879e8aef2eae3d0231af757e1e5f4
GuillaumeLoriot <loriot.guillaume@gmail.com>
2025-08-14 11:20:52 +0200

    finished static homepage, missing 2 dynamic component(announcements and reviews)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { AnnouncementsComponent } from './components/announcements/announcements.component';
+import { HelpComponent } from './components/help/help.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'contact', component: ContactComponent },
+    { path: 'announcements', component: AnnouncementsComponent },
+    { path: 'help', component: HelpComponent },
+    { path: 'login', component: LoginComponent },
 ];
