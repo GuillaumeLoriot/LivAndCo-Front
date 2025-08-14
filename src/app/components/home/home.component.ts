8c7f450b914bf2f97ede2717e562754de3dffad5
GuillaumeLoriot <loriot.guillaume@gmail.com>
2025-08-14 19:07:40 +0200

    created slider + style, need logic implementation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ReviewSliderComponent } from "../review-slider/review-slider.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReviewSliderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/components/review-slider/review-slider.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-slider',
+  standalone: true,
+  imports: [],
+  templateUrl: './review-slider.component.html',
+  styleUrl: './review-slider.component.scss'
+})
+export class ReviewSliderComponent {
+
+  tests = [1,2,3];
+
+}
