41387d453ccfb689f058b5b0f95c89b590aa2d81
GuillaumeLoriot <loriot.guillaume@gmail.com>
2025-08-26 00:05:22 +0200

    finished announcement list componnent templates

---
+++ src/app/components/common/results-map/results-map.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-results-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './results-map.component.html',
+  styleUrl: './results-map.component.scss'
+})
+export class ResultsMapComponent {
+
+}
--- src/app/components/pages/announcement-list/announcement-list.component.ts
+++ src/app/components/pages/announcement-list/announcement-list.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { SearchBarComponent } from '../../common/search-bar/search-bar.component';
 import { CommonModule } from '@angular/common';
+import { AnnouncementListCardComponent } from "../../common/announcement-list-card/announcement-list-card.component";
+import { ResultsMapComponent } from "../../common/results-map/results-map.component";
 
 
 @Component({
   selector: 'app-announcement-list',
   standalone: true,
-  imports: [CommonModule,SearchBarComponent],
+  imports: [CommonModule, SearchBarComponent, AnnouncementListCardComponent, ResultsMapComponent],
   templateUrl: './announcement-list.component.html',
   styleUrl: './announcement-list.component.scss'
 })
