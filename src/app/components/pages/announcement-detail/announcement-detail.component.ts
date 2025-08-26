0cfaca210d1e13de570c00ac318f38bed34b7d6d
GuillaumeLoriot <loriot.guillaume@gmail.com>
2025-08-26 14:32:33 +0200

    finished announcement detail page

--- src/app/components/pages/announcement-detail/announcement-detail.component.ts
+++ src/app/components/pages/announcement-detail/announcement-detail.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ResultsMapComponent } from "../../common/results-map/results-map.component";
 import { ReviewSliderComponent } from "../../common/review-slider/review-slider.component";
+import { ReservationCardComponent } from "../../common/reservation-card/reservation-card.component";
 
 @Component({
   selector: 'app-announcement-detail',
   standalone: true,
-  imports: [ResultsMapComponent, ReviewSliderComponent],
+  imports: [ResultsMapComponent, ReviewSliderComponent, ReservationCardComponent],
   templateUrl: './announcement-detail.component.html',
   styleUrl: './announcement-detail.component.scss'
 })
