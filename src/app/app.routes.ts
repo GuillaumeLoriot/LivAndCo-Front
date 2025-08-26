8c5b5fb90ca2050c2664c93550c9fb7b3503761e
GuillaumeLoriot <loriot.guillaume@gmail.com>
2025-08-27 00:31:31 +0200

    finished reservation, login, register, terms of use and privacy policy templates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,10 @@ import { HelpComponent } from './components/pages/help/help.component';
 import { LoginComponent } from './components/pages/login/login.component';
 import { AnnouncementListComponent } from './components/pages/announcement-list/announcement-list.component';
 import { AnnouncementDetailComponent } from './components/pages/announcement-detail/announcement-detail.component';
+import { ReservationComponent } from './components/pages/reservation/reservation.component';
+import { RegisterComponent } from './components/pages/register/register.component';
+import { PrivacyPolicyComponent } from './components/pages/privacy-policy/privacy-policy.component';
+import { TermsOfUseComponent } from './components/pages/terms-of-use/terms-of-use.component';
 
 
 export const routes: Routes = [
@@ -12,7 +16,11 @@ export const routes: Routes = [
     { path: 'contact', component: ContactComponent },
     { path: 'announcements', component: AnnouncementListComponent },
     { path: 'announcements/:id', component: AnnouncementDetailComponent },
+    { path: 'announcements/:id/reservation', component: ReservationComponent },
     { path: 'help', component: HelpComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent },
+    { path: 'terms-of-use', component: TermsOfUseComponent },
 
 ];
--- src/app/components/common/reservation-card/reservation-card.component.ts
+++ src/app/components/common/reservation-card/reservation-card.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-reservation-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './reservation-card.component.html',
   styleUrl: './reservation-card.component.scss'
 })
