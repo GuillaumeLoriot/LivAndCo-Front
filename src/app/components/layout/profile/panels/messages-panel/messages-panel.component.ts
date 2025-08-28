2f0137b8ef855a1833baca602b0a374d93f20747
GuillaumeLoriot <loriot.guillaume@gmail.com>
2025-08-29 00:41:32 +0200

    updated hiden class in messages

--- src/app/components/layout/profile/panels/messages-panel/messages-panel.component.ts
+++ src/app/components/layout/profile/panels/messages-panel/messages-panel.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { PanelStateService } from '../../../../../services/utils/panel-state.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-messages-panel',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule,RouterLink],
   templateUrl: './messages-panel.component.html',
   styleUrl: './messages-panel.component.scss'
 })
 export class MessagesPanelComponent {
-
+ constructor(public panelState: PanelStateService) { }
 }
