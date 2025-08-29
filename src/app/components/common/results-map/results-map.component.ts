9ace24e9189fe855740ad61631350a57a8cdd4e0
GuillaumeLoriot <loriot.guillaume@gmail.com>
2025-08-29 10:24:58 +0200

    updated config for tailwind. switched to v3 because of conflicts with maplibre

--- src/app/components/common/results-map/results-map.component.ts
+++ src/app/components/common/results-map/results-map.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NgxMapLibreGLModule } from '@maplibre/ngx-maplibre-gl';
 
 @Component({
   selector: 'app-results-map',
   standalone: true,
-  imports: [],
+  imports: [NgxMapLibreGLModule],
   templateUrl: './results-map.component.html',
   styleUrl: './results-map.component.scss'
 })
