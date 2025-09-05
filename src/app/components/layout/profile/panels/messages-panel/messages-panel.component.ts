8edca65ef0917f6510d8315c7cada55e54d942d7
GuillaumeLoriot <loriot.guillaume@gmail.com>
2025-09-05 02:52:04 +0200

    made message service an message pannel to display real data

--- src/app/components/layout/profile/panels/messages-panel/messages-panel.component.ts
+++ src/app/components/layout/profile/panels/messages-panel/messages-panel.component.ts
@@ -1,15 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { PanelStateService } from '../../../../../services/utils/panel-state.service';
 import { CommonModule } from '@angular/common';
+import { MessageService } from '../../../../../services/message/message.service';
+import User from '../../../../../models/user.interface';
+import Message from '../../../../../models/message.interface';
+import { UserService } from '../../../../../services/user/user.service';
 
 @Component({
   selector: 'app-messages-panel',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './messages-panel.component.html',
   styleUrl: './messages-panel.component.scss'
 })
-export class MessagesPanelComponent {
- constructor(public panelState: PanelStateService) { }
+export class MessagesPanelComponent implements OnInit{
+
+
+  private messageService = inject(MessageService);
+  private userService = inject(UserService);
+
+  peers: User[] = [];
+  isLoading = false;
+  error = false;
+  errorMessage: string | null = null;
+  connectedUser: User | null = null;
+  myId!: number;
+
+  constructor(public panelState: PanelStateService) { }
+
+
+  ngOnInit(): void {
+    this.loadUser();
+    
+  }
+
+  loadDistinctPeers(myId: number): void {
+    this.isLoading = true;
+
+    this.messageService.getMessages().subscribe({
+      next: (messages: Message[]) => {
+        const uniquePeersById = new Map<number, User>();
+
+        for (const message of messages) {
+          const other =
+            message.sender?.id === myId ? message.receiver :
+              message.receiver?.id === myId ? message.sender : null;
+
+          if (other && !uniquePeersById.has(other.id)) {
+            uniquePeersById.set(other.id, other);
+          }
+        }
+
+        this.peers = Array.from(uniquePeersById.values());
+        console.log(this.peers)
+        this.isLoading = false;
+      },
+      error: () => {
+        this.error = true;
+        this.isLoading = false;
+      }
+    });
+  }
+
+  loadUser() {
+    this.isLoading = true;
+    this.userService.getUser().subscribe({
+      next: (data) => { 
+        this.connectedUser = data; 
+        this.myId = data.id;
+        console.log(this.connectedUser);
+        this.loadDistinctPeers(this.myId); 
+        this.isLoading = false; },
+      error: (error) => {
+        // Affichage de l'erreur dans la template
+        if (error.status) {
+          this.errorMessage = error.error?.message;
+        } else {
+          this.errorMessage = "Une erreur est survenue. Veuillez réessayer.";
+        }
+        this.error = true;
+        this.isLoading = false;
+      }
+    });
+  }
+
 }
---
+++ src/app/services/message/message.service.ts
@@ -0,0 +1,43 @@
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import Message from '../../models/message.interface';
+import { enableAuthContext } from '../../interceptors/authentification/auth.interceptor';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  constructor() { }
+  private apiUrl = `${environment.apiUrl}/messages`;
+  private http = inject(HttpClient);
+
+
+  getMessages(): Observable<Message[]> {
+    return this.http.get<Message[]>(this.apiUrl, {
+      headers: { 'accept': 'application/json' },
+      context: enableAuthContext(),
+    });
+  }
+
+
+
+  // Récupère tous les message du user connecté qui corresponde au user recherché (peer) (traité coté back via une queryExtension)
+  getConversation(peerId: number): Observable<Message[]> {
+
+    const params = new HttpParams()
+      .set('peer', String(peerId))
+      .set('order[createdAt]', 'asc');
+
+    return this.http.get<Message[]>(this.apiUrl, {
+      params,
+      headers: { accept: 'application/json' },
+      // Permet de dire qu'il faut que l'interceptor ajoute le token pour cette requète
+      context: enableAuthContext(),
+    });
+  }
+
+
+}
