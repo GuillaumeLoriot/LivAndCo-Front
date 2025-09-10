983a0e790ce790f6e35ab934c5c3df462eace471
GuillaumeLoriot <loriot.guillaume@gmail.com>
2025-09-10 15:46:21 +0200

    fix environment

--- src/app/services/convenience/convenience.service.ts
+++ src/app/services/convenience/convenience.service.ts
@@ -1,8 +1,8 @@
 import { inject, Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import Convenience from '../../models/convenience.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
